9fbd58f5eee484934ade6ae12d5615bad320e435
AnpanPat <74598695+AnpanPat@users.noreply.github.com>
2021-03-29 11:22:48 +0200

    Schedule

--- cinemaApp/Program.cs
+++ cinemaApp/Program.cs
@@ -70,6 +70,7 @@ namespace cinemaApp
             Console.WriteLine("1. View movies");
             Console.WriteLine("2. View reviews");
             Console.WriteLine("3. View catering");
+            Console.WriteLine("4. View Schedule");
             var options = Console.ReadLine();
             if (options == "1")
             {
@@ -85,6 +86,10 @@ namespace cinemaApp
                 Console.WriteLine("The catering section is not available yet");
                 Console.Read();
             }
+            else if (options == "4")
+            {
+                Schedule();
+            }
         }
         private static void adminScreen()
         {
@@ -94,8 +99,29 @@ namespace cinemaApp
             Console.WriteLine("1. Manage movies");
             Console.WriteLine("2. Manage reservations");
             Console.WriteLine("3. Manage catering");
+            Console.WriteLine("4. Manage Schedule");
         }
+        private static void Schedule()
+        {
+            Console.Clear();
+            Console.WriteLine("Minari:\n 19:00 | 21:00 | 23:00 | 01:00 | 03:00\n");
+            Console.WriteLine("Sound of metal:\n 18:30 | 20:30 | 22:30 | 00:30 | 02:00\n");
+            Console.WriteLine("Normadland:\n 18:00 | 22:00 | 22:30 | 01:00 | 02:00\n");
+            Console.WriteLine("Another round:\n 13:00| 15:30 | 19:00 | 21:00 | 23:00 | 03:00 |\n");
+            Console.WriteLine("The Father:\n 12:45 | 15:00 | 19:00 | 21:00 | 23:00\n");
+            Console.WriteLine("Do you want to go to the film selection screeen? y/n: ");
+            var nis = Console.ReadLine();
+            if (nis == "Y" | nis == "y")
+            {
+                
+                FilmInfoScreen();
+            }
+            else if (nis == "N" | nis == "n")
+            {
+                mainScreen();
+            }
 
+        }
         private static bool accountchecker(string usernameORpassword)
         {
             if (usernameORpassword == "customer" | usernameORpassword == "123")
