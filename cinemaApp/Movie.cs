6f3115e490ca40c463a010ffabc47c49bcfe8817
Silvijn de Maat <1006512@hr.nl>
2021-05-17 17:33:53 +0200

    added movie reservations

--- cinemaApp/Movie.cs
+++ cinemaApp/Movie.cs
@@ -10,8 +10,9 @@
         public int AgeRating { get; set; }
         public double IMDB { get; set; }
         public string Synopsis { get; set; }
-        
-        public Movie(int id,string title, string genre, string language, double runtime, int agerating, double imdb, string synopsis)
+        public double Price { get; set; }
+
+        public Movie(int id,string title, string genre, string language, double runtime, int agerating, double imdb, string synopsis, double price)
         {
             ID = id;
             Title = title;
@@ -21,13 +22,14 @@
             AgeRating = agerating;
             IMDB = imdb;
             Synopsis = synopsis;
+            Price = price;
         }
         
         
         
         public override string ToString()
         {
-            return string.Format("{0}\nTitle: {1}\nGenre: {2}\nLanguage: {3}\nRuntime: {4}\nAge rating: {5}\nImdb score: {6}\n\nSynopsis: {7}\n\n",ID, Title, Genre, Language, Runtime, AgeRating, IMDB, Synopsis);
+            return string.Format("{0}\nTitle: {1}\nGenre: {2}\nLanguage: {3}\nRuntime: {4}\nAge rating: {5}\nImdb score: {6}\n\nSynopsis: {7}\nPrice: {8}\n\n",ID, Title, Genre, Language, Runtime, AgeRating, IMDB, Synopsis, Price);
         }
     }
 }
--- cinemaApp/MovieInfoScreen.cs
+++ cinemaApp/MovieInfoScreen.cs
@@ -7,112 +7,6 @@ namespace cinemaApp
 {
     class MovieInfoScreen
     {
-
-        public static void reserve(Account currentAccount)
-        {
-
-        }
-        public static void showMovies(Account CurrentAccount)
-        {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Green;
-            string a = "/// FILM SELECTION ///\n";
-            Console.SetCursorPosition((Console.WindowWidth - a.Length) / 2, Console.CursorTop);
-            Console.WriteLine(a);
-            Console.ResetColor();
-            bool choosing = true;
-                List<String> jsonContents = new List<String> { };
-                try
-                {
-                    foreach (string line in File.ReadLines(@"movies.json")) //Creates a list with every object from json file
-                    {
-                        jsonContents.Add(line);
-                    }
-                    var movieList = new List<Movie> { };
-                    foreach (String movie in jsonContents)
-                    {
-                        movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
-                    }
-                    foreach (var movie in movieList)
-                    {
-                    Console.WriteLine(movie);
-                    }
-                }
-                catch (FileNotFoundException)
-                {
-                    Console.WriteLine("\nNo movies found!\nPlease create a listing first!\n");
-                    System.Threading.Thread.Sleep(3500);
-                    Console.Clear();
-                    mainScreen.Show(CurrentAccount);
-                }
-
-                Console.WriteLine("Would you like to make a reservation?\n1. Yes\n2. No\n");
-                string options = Console.ReadLine();
-                try
-                {
-                    int number = Int32.Parse(options);
-                    switch (number)
-                    {
-                        case 1:
-                        try
-                        {
-                            while (choosing)
-                            {
-                                Console.WriteLine("\nPlease enter the movie number\n");
-                                string choice = Console.ReadLine();
-                                int result = Int32.Parse(choice);
-                               
-
-                                var movieList = new List<Movie> { };
-                                foreach (String movie in jsonContents)
-                                {
-                                    movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
-                                }
-                                foreach (var movie in movieList)
-                                {
-                                    if(movie.ID == result)
-                                    {
-                                        movieSelecter(result);
-                                        choosing = false;
-                                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                                        Console.WriteLine("\nYou selected this film! but the reservation section is currently under construction\nYou will be send back to the mainscreen");
-                                        System.Threading.Thread.Sleep(7000);
-                                        Console.ResetColor();
-                                        Console.Clear();
-                                        mainScreen.Show(CurrentAccount);
-                                        break;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("There is no movie with that Index, please try again.");
-                                        break;
-                                    }
-                                }  
-                            }
-                        }
-                        catch
-                        {
-                            Console.WriteLine("There is no movie with that Index, please try again.");
-                        }
-                            break;
-                        case 2:
-                            Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.WriteLine("\nYou will be send back to the mainscreen\n");
-                            Console.ResetColor();
-                            System.Threading.Thread.Sleep(2000);
-                            Console.Clear();
-                            mainScreen.Show(CurrentAccount);
-                            break;
-                        default:
-                            Console.WriteLine("The input you gave is incorrect.");
-                            break;
-                    }
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Please enter a number");
-                }
-        }
         public static void movieSelecter(int option)
         {
             Console.Clear();
@@ -140,5 +34,121 @@ namespace cinemaApp
                     Console.WriteLine("Please enter a number");
                 }
         }
+        public static void reserve(Account currentAccount)
+        {
+
+        }
+
+
+        public static void showMovies(Account CurrentAccount)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            string a = "/// FILM SELECTION ///\n";
+            Console.SetCursorPosition((Console.WindowWidth - a.Length) / 2, Console.CursorTop);
+            Console.WriteLine(a);
+            Console.ResetColor();
+            bool choosing = true;
+                List<String> jsonContents = new List<String> { };
+                try
+                {
+                    foreach (string line in File.ReadLines(@"movies.json")) //Creates a list with every object from json file
+                    {
+                        jsonContents.Add(line);
+                    }
+                    var movieList = new List<Movie> { };
+                    foreach (String movie in jsonContents)
+                    {
+                        movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
+                    }
+                    foreach (var movie in movieList)
+                    {
+                    Console.WriteLine(movie);
+                    }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("\nNo movies found!\nPlease create a listing first!\n");
+                System.Threading.Thread.Sleep(3500);
+                Console.Clear();
+               mainScreen.Show(CurrentAccount);
+            }
+
+            Console.WriteLine("Would you like to make a reservation?\n1. Yes\n2. No\n");
+                string options = Console.ReadLine();
+                try
+                {
+                    int number = Int32.Parse(options);
+                    switch (number)
+                    {
+                        case 1:
+                        try
+                        {
+                            while (choosing)
+                            {
+                                Console.WriteLine("\nPlease enter the movie number\n");
+                                string choice = Console.ReadLine();
+                                int result = Int32.Parse(choice);
+
+                                var movieList = new List<Movie> { };
+                                foreach (String movie in jsonContents)
+                                {
+                                    movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
+                                }
+
+                                foreach (var movie in movieList)
+                                {
+                                    if(movie.ID == result)
+                                    {
+                                        Cart newCartJSON = new Cart(CurrentAccount.ID, movie.Title, movie.Price);
+                                        string strNewCartJSON = JsonConvert.SerializeObject(newCartJSON);
+                                        using (StreamWriter sw = File.AppendText(@"cart.json"))
+                                        {
+                                            sw.WriteLine(strNewCartJSON);
+                                            sw.Close();
+                                        }
+                                        movieSelecter(result);
+                                        choosing = false;
+                                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+                                        Console.WriteLine("\nThe movie has been added to your basket!\nYou will be send back to the mainscreen");
+                                        System.Threading.Thread.Sleep(5000);
+                                        Console.ResetColor();
+                                        Console.Clear();
+                                        mainScreen.Show(CurrentAccount);
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("There is no movie with that number, please try again.");
+                                        break;
+                                    }
+                                }  
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("There is no movie with that number, please try again.");
+                        }
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("\nYou will be send back to the mainscreen\n");
+                            Console.ResetColor();
+                            System.Threading.Thread.Sleep(2000);
+                            Console.Clear();
+                            mainScreen.Show(CurrentAccount);
+                            break;
+                        default:
+                            Console.WriteLine("The input you gave is incorrect.");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+        }
+        
     }
 }
