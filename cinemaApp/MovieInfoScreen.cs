ab61a9996fafb097fc4b5d5ccf30b85a9f889290
Matthijs Vos <matthijsdell1@outlook.com>
2021-05-25 15:03:40 +0200

    asfasfsf

--- cinemaApp/MovieInfoScreen.cs
+++ cinemaApp/MovieInfoScreen.cs
@@ -21,45 +21,47 @@ namespace cinemaApp
             Console.WriteLine(a);
             Console.ResetColor();
             bool choosing = true;
-                List<String> jsonContents = new List<String> { };
-                try
+            List<String> jsonContents = new List<String> { };
+            try
+            {
+                foreach (string line in File.ReadLines(@"movies.json")) //Creates a list with every object from json file
                 {
-                    foreach (string line in File.ReadLines(@"movies.json")) //Creates a list with every object from json file
-                    {
-                        jsonContents.Add(line);
-                    }
-                    var movieList = new List<Movie> { };
-                    foreach (String movie in jsonContents)
-                    {
-                        movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
-                    }
-                    foreach (var movie in movieList)
-                    {
+                    jsonContents.Add(line);
+                }
+                var movieList = new List<Movie> { };
+                foreach (String movie in jsonContents)
+                {
+                    movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
+                }
+                foreach (var movie in movieList)
+                {
                     Console.WriteLine(movie);
-                    }
                 }
-                catch (FileNotFoundException)
-                {
-                    Console.WriteLine("\nNo movies found!\nPlease create a listing first!\n");
-                    System.Threading.Thread.Sleep(3500);
-                    Console.Clear();
-                    mainScreen.Show(CurrentAccount);
-                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("\nNo movies found!\nPlease create a listing first!\n");
+                System.Threading.Thread.Sleep(3500);
+                Console.Clear();
+                mainScreen.Show(CurrentAccount);
+            }
 
-                Console.WriteLine("Would you like to make a reservation and see the reviews of a selected film?\n1. Yes\n2. No\n");
-                string options = Console.ReadLine();
-                try
+            Console.WriteLine("Would you like to make a reservation and see the reviews of a selected film?\n1. Yes\n2. No\n");
+            string options = Console.ReadLine();
+            try
+            {
+                int number = Int32.Parse(options);
+                switch (number)
                 {
-                    int number = Int32.Parse(options);
-                    switch (number)
-                    {
-                        case 1:
+                    case 1:
+                        try
+                        {
                             while (choosing)
                             {
                                 Console.WriteLine("\nPlease enter the movie number\n");
                                 string choice = Console.ReadLine();
                                 int result = Int32.Parse(choice);
-                               
+
 
                                 var movieList = new List<Movie> { };
                                 foreach (String movie in jsonContents)
@@ -106,31 +108,31 @@ namespace cinemaApp
                                             Console.WriteLine("Please enter a number");
                                         }
                                     }
-                                }  
+                                }
                             }
                         }
                         catch
                         {
                             Console.WriteLine("There is no movie with that Index, please try again.");
                         }
-                            break;
-                        case 2:
-                            Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.WriteLine("\nYou will be send back to the mainscreen\n");
-                            Console.ResetColor();
-                            System.Threading.Thread.Sleep(2000);
-                            Console.Clear();
-                            mainScreen.Show(CurrentAccount);
-                            break;
-                        default:
-                            Console.WriteLine("The input you gave is incorrect.");
-                            break;
-                    }
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("\nYou will be send back to the mainscreen\n");
+                        Console.ResetColor();
+                        System.Threading.Thread.Sleep(2000);
+                        Console.Clear();
+                        mainScreen.Show(CurrentAccount);
+                        break;
+                    default:
+                        Console.WriteLine("The input you gave is incorrect.");
+                        break;
                 }
-                catch (Exception)
-                {
-                    Console.WriteLine("Please enter a number");
-                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a number");
+            }
         }
         public static void movieSelecter(int option)
         {
@@ -140,47 +142,47 @@ namespace cinemaApp
             Console.SetCursorPosition((Console.WindowWidth - a.Length) / 2, Console.CursorTop);
             Console.WriteLine(a);
             Console.ResetColor();
-                List<String> jsonContents = new List<String> { };
-                try
+            List<String> jsonContents = new List<String> { };
+            try
+            {
+                foreach (string line in File.ReadLines(@"movies.json")) //Creates a list with every object from json file
                 {
-                    foreach (string line in File.ReadLines(@"movies.json")) //Creates a list with every object from json file
-                    {
-                        jsonContents.Add(line);
-                    }
-                    var movieList = new List<Movie> { };
-                    foreach (String movie in jsonContents)
-                    {
-                        movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
-                    }
-                    Console.WriteLine(movieList[option - 1]);
+                    jsonContents.Add(line);
+                }
+                var movieList = new List<Movie> { };
+                foreach (String movie in jsonContents)
+                {
+                    movieList.Add(JsonConvert.DeserializeObject<Movie>(movie));
+                }
+                Console.WriteLine(movieList[option - 1]);
 
-                }
-                catch (Exception)
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a number");
+            }
+            List<String> jsonCuntents = new List<String> { };
+            try
+            {
+
+                foreach (string line in File.ReadLines(@"reviews.json"))
                 {
-                    Console.WriteLine("Please enter a number");
+                    jsonCuntents.Add(line);
                 }
-                List<String> jsonCuntents = new List<String> { };
-                try
+                var reviewList = new List<ReviewJSN> { };
+                foreach (String review in jsonCuntents)
                 {
-                    
-                    foreach (string line in File.ReadLines(@"reviews.json")) 
-                    {
-                        jsonCuntents.Add(line);
-                    }
-                    var reviewList = new List<ReviewJSN> { };
-                    foreach (String review in jsonCuntents)
-                    {
-                        reviewList.Add(JsonConvert.DeserializeObject<ReviewJSN>(review));
-                    }
-                    foreach (var review in reviewList)
-                    {
+                    reviewList.Add(JsonConvert.DeserializeObject<ReviewJSN>(review));
+                }
+                foreach (var review in reviewList)
+                {
                     Console.WriteLine(review);
-                    }
                 }
-                catch (Exception)
-                {
-                    Console.WriteLine("Please enter a number");
-                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a number");
+            }
         }
     }
 }
